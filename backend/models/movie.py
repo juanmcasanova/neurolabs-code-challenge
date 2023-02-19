7f3339a7e50aae85b8a0ab7a6afc8bac5acb8bc3
Juan <juan.casanova.922@gmail.com>
2023-02-19 18:02:56 +0000

    Add mandatory length

--- backend/models/movie.py
+++ backend/models/movie.py
@@ -6,4 +6,4 @@ class Movie(Base):
     __tablename__ = "movies"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
+    title = Column(String(255), index=True)
