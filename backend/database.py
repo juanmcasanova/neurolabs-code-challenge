637cf6e4511f0c2bfefb6073c28f091121767c3d
Juan <juan.casanova.922@gmail.com>
2023-02-19 18:50:27 +0000

    Use non-deprecated version of declarative_base

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,7 @@
 import os
 
-from sqlalchemy                 import create_engine, Engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm             import sessionmaker, Session
+from sqlalchemy     import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker, Session
 
 engine       = create_engine(os.environ['DATABASE_URL'])
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- backend/models/movie.py
+++ backend/models/movie.py
@@ -1,5 +1,5 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy                 import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+from sqlalchemy     import Column, Integer, String
 
 Base = declarative_base()
 
