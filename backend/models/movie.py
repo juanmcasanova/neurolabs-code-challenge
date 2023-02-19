3d1d5abf1f2f50a434947c458aa6a375a066c0db
Juan <juan.casanova.922@gmail.com>
2023-02-19 18:12:47 +0000

    Add and configure Alembic for migrations

--- backend/models/movie.py
+++ backend/models/movie.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy                 import Column, Integer, String
 
-from ..database import Base
+Base = declarative_base()
 
 class Movie(Base):
     __tablename__ = "movies"
