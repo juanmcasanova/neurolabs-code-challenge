65f6cd45a771edc6cf7503d419c9031fae98c12a
Juan <juan.casanova.922@gmail.com>
2023-02-19 18:01:17 +0000

    Remove parameter specific to SQLite

--- backend/database.py
+++ backend/database.py
@@ -4,7 +4,7 @@ from sqlalchemy                 import create_engine, Engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm             import sessionmaker, Session
 
-engine       = create_engine(os.environ['DATABASE_URL'], connect_args={"check_same_thread": False})
+engine       = create_engine(os.environ['DATABASE_URL'])
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base         = declarative_base()
 
