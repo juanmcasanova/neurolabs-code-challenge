87327c2a092b3160e6cb69fc07e07e5965319f99
Juan <juan.casanova.922@gmail.com>
2023-02-19 18:02:45 +0000

    Remove unnecessary slash

--- backend/routers/movies.py
+++ backend/routers/movies.py
@@ -7,6 +7,6 @@ from ..repositories import movies as movies_repository
 
 router = APIRouter(prefix="/movies")
 
-@router.get("/", response_model=list[movie_schemas.Movie])
+@router.get("", response_model=list[movie_schemas.Movie])
 def read_movies(skip: int = 0, limit: int = 10, db: Session = Depends(get_db_session)):
     return movies_repository.get_users(db, skip=skip, limit=limit)
